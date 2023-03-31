4d52359776f3a49ed8273826d9f9716bbffc2bb9
martinogilli <marty.gilli@gmail.com>
2023-03-31 14:57:34 +0200

    Added avoided damages plots

--- plot_impacts.R
+++ plot_impacts.R
@@ -2,10 +2,11 @@
 colors_deciles <- c("purple", rev(colorspace::diverging_hcl(9, "Cyan-Magenta")))
 
 #### GDP damages ####
-reg_dam_plot = function(spec, dir ) {
+reg_dam_plot = function(spec, dir) {
   
   if(spec == "BHM") {
     
+    # country-level damages
     title <- expression(atop(bold("Damages on GDP, across countries"),
                              scriptstyle("Without adaptation")))
     
@@ -23,11 +24,43 @@ reg_dam_plot = function(spec, dir ) {
       theme(plot.title = element_text(hjust = 0.5),
             legend.position = "bottom")
     
+    # avoided country-level damages by going from REF to 650
+    title_2 <- expression(atop(bold("Avoided damages from REF to 650, across countries"),
+                             scriptstyle("Without adaptation")))
+    
+    p_reg_avoided_dam <- ggplot(mip_income_d %>% 
+                          na.omit() %>% 
+                          filter(Scenario == "REF" | Scenario == "650") %>% 
+                            select(Scenario:Year, Reg_damages_bhm_frac) %>% 
+                            select(-Scenario_type) %>% 
+                            distinct() %>% 
+                            mutate(
+                              Reg_damages_bhm_frac = Reg_damages_bhm_frac*100
+                            ) %>% 
+                            pivot_wider(
+                              names_from = Scenario,
+                              values_from = Reg_damages_bhm_frac
+                            ) %>% 
+                            mutate(
+                              avoided_reg_dam_bhm = REF - `650`
+                            ),
+                        aes(x = Year, y = avoided_reg_dam_bhm,
+                            color = Model)) +
+      geom_line(linewidth = 1) +
+      labs(y = "Avoided damages, %",
+           title = title_2) +
+      xlim(c(2020, 2100)) +
+      facet_wrap(~ Region, ncol = 5) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5),
+            legend.position = "bottom")
+    
     
   }
   
   if(spec == "Adaptation") {
     
+    # Country-level damages
     title <- expression(atop(bold("Damages on GDP, across countries")))
   
     
@@ -44,13 +77,50 @@ reg_dam_plot = function(spec, dir ) {
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5),
             legend.position = "bottom")
+    
+    # avoided country-level damages by going from REF to 650
+    title_2 <- expression(atop(bold("Avoided damages from REF to 650, across countries"),
+                               scriptstyle("Without adaptation")))
+    
+    p_reg_avoided_dam <- ggplot(mip_income_d %>% 
+                                  na.omit() %>% 
+                                  filter(Scenario == "REF" | Scenario == "650") %>% 
+                                  select(Scenario:Year, Reg_damages_ada_frac) %>% 
+                                  select(-Scenario_type) %>% 
+                                  distinct() %>% 
+                                  mutate(
+                                    Reg_damages_ada_frac = Reg_damages_ada_frac*100
+                                  ) %>% 
+                                  pivot_wider(
+                                    names_from = Scenario,
+                                    values_from = Reg_damages_ada_frac
+                                  ) %>% 
+                                  mutate(
+                                    avoided_reg_dam_ada = REF - `650`
+                                  ),
+                                aes(x = Year, y = avoided_reg_dam_ada,
+                                    color = Model)) +
+      geom_line(linewidth = 1) +
+      labs(y = "Avoided damages, %",
+           title = title_2) +
+      xlim(c(2020, 2100)) +
+      facet_wrap(~ Region, ncol = 5) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5),
+            legend.position = "bottom")
   }
   
   ggsave(filename = paste0(spec, "_region_damages_all.png"),
+         plot = p_reg_dam,
          width = 15, height = 7, path = dir)
 
-  return(p_reg_dam)
-
+  ggsave(filename = paste0(spec, "_region_avoid_damages_all.png"),
+         plot = p_reg_avoided_dam,
+         width = 15, height = 7, path = dir)
+  
+  
+  return(list(p_reg_dam, p_reg_avoided_dam))
+  
 }
 
 reg_dam_plot(spec = "BHM", dir = "graphs")
@@ -61,6 +131,7 @@ gini_plot = function(spec, dir) {
   
   if(spec == "BHM") {
     
+    # Country-level impacts on Gini
     title <- expression(atop(bold("Impacts on Gini index, across countries"),
                              scriptstyle("Without adaptation")))
     
@@ -78,11 +149,42 @@ gini_plot = function(spec, dir) {
       theme(plot.title = element_text(hjust = 0.5),
             legend.position = "bottom")
     
+    # Avoided Gini impacts from REF to 650 scenario
+    title_2 <- expression(atop(bold("Avoided impacts on Gini index from REF to 650, across countries"),
+                               scriptstyle("Without adaptation")))
+    
+    p_avoided_gini <- ggplot(mip_income_d %>% 
+                                  na.omit() %>% 
+                                  filter(Scenario == "REF" | Scenario == "650") %>% 
+                                  select(Scenario:Year, delta_gini_bhm) %>% 
+                                  select(-Scenario_type) %>% 
+                                  distinct() %>% 
+                                  mutate(
+                                    delta_gini_bhm = delta_gini_bhm*100
+                                  ) %>% 
+                                  pivot_wider(
+                                    names_from = Scenario,
+                                    values_from = delta_gini_bhm
+                                  ) %>% 
+                                  mutate(
+                                    avoided_gini_bhm = REF - `650`
+                                  ),
+                                aes(x = Year, y = avoided_gini_bhm,
+                                    color = Model)) +
+      geom_line(linewidth = 1) +
+      labs(y = "Avoided \u0394 Gini",
+           title = title_2) +
+      xlim(c(2020, 2100)) +
+      facet_wrap(~ Region, ncol = 5) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5),
+            legend.position = "bottom")
     
   }
   
   if(spec == "Adaptation") {
     
+    # Country-level impacts on Gini index
     title <- expression(atop(bold("Impacts on Gini index, across countries")))
     
     
@@ -99,19 +201,54 @@ gini_plot = function(spec, dir) {
       theme_bw() +
       theme(plot.title = element_text(hjust = 0.5),
             legend.position = "bottom")
+    
+    # Avoided Gini impacts from REF to 650 scenario
+    title_2 <- expression(atop(bold("Avoided impacts on Gini index from REF to 650, across countries")))
+    
+    p_avoided_gini <- ggplot(mip_income_d %>% 
+                                  na.omit() %>% 
+                                  filter(Scenario == "REF" | Scenario == "650") %>% 
+                                  select(Scenario:Year, delta_gini_ada) %>% 
+                                  select(-Scenario_type) %>% 
+                                  distinct() %>% 
+                                  mutate(
+                                    delta_gini_ada = delta_gini_ada*100
+                                  ) %>% 
+                                  pivot_wider(
+                                    names_from = Scenario,
+                                    values_from = delta_gini_ada
+                                  ) %>% 
+                                  mutate(
+                                    avoided_gini_ada = REF - `650`
+                                  ),
+                                aes(x = Year, y = avoided_gini_ada,
+                                    color = Model)) +
+      geom_line(linewidth = 1) +
+      labs(y = "Avoided \u0394 Gini",
+           title = title_2) +
+      xlim(c(2020, 2100)) +
+      facet_wrap(~ Region, ncol = 5) +
+      theme_bw() +
+      theme(plot.title = element_text(hjust = 0.5),
+            legend.position = "bottom")
   }
   
   ggsave(filename = paste0(spec, "_gini_impacts_all.png"),
+         plot = p_gini,
          width = 15, height = 7, path = dir)
   
-  return(p_gini)
+  ggsave(filename = paste0(spec, "_gini_avoided_impacts_all.png"),
+         plot = p_avoided_gini,
+         width = 15, height = 7, path = dir)
+  
+  return(list(p_gini, p_avoided_gini))
   
 }
 
 gini_plot(spec = "BHM", dir = "graphs")
 gini_plot(spec = "Adaptation", dir = "graphs")
 
-# produce regression table of Delta Gini on Delta regional temperature
+# produces regression table of Delta Gini on Delta regional temperature
 
 gini_temp_reg = function(spec, dir) {
 
@@ -181,11 +318,11 @@ decile_plot = function (spec, dir) {
   
   if(spec == "BHM") {
     
+    # Decile-level impacts
     title <- expression(atop(bold("Damages at decile-level, across countries"),
                              scriptstyle("Without adaptation")))
   
    
-    
   p_dec_dam <-  ggplot(mip_income_d %>% 
                          filter(Scenario == "REF" | Scenario == "650"
                          ) %>% 
@@ -206,11 +343,46 @@ decile_plot = function (spec, dir) {
           legend.position="bottom",
           plot.title = element_text(hjust = 0.5))
   
-
-  }
+  
+  # Avoided decile-level impacts from REF to 650
+  title_2 <- expression(atop(bold("Avoided damages at decile-level from REF to 650")),
+  scriptstyle("Without adaptation"))
+  
+  p_dec_avoided_dam <-  ggplot(mip_income_d %>% 
+    na.omit() %>% 
+    filter(Scenario == "REF" | Scenario == "650") %>% 
+    select(Scenario:Year, Decile, dam_dist_frac_bhm) %>% 
+    select(-Scenario_type) %>% 
+    distinct() %>% 
+    mutate(
+      dam_dist_frac_bhm = dam_dist_frac_bhm*100,
+      Decile = factor(Decile,
+                      levels = c("D1", "D2", "D3", "D4", "D5",
+                                 "D6", "D7", "D8", "D9", "D10"))
+    ) %>% 
+    pivot_wider(
+      names_from = Scenario,
+      values_from = dam_dist_frac_bhm
+    ) %>% 
+    mutate(avoided_reg_dam_bhm = REF - `650`) ,
+                       aes(x = Year, y = avoided_reg_dam_bhm,
+                           color = Decile, shape = Model)) +
+    geom_line(alpha = 0.5) +
+    geom_point() +
+    labs(x = "Year", y = "Avoided damages, %", title = title_2) +
+    scale_color_manual(name = "Income deciles", values = colors_deciles) +
+    scale_shape_manual(name = "Models", values = mod_letters_utf) +
+    facet_wrap(~ Region, ncol = 5) +
+    scale_x_continuous(limits = c(2020, 2100), breaks = c(2020, 2060, 2100)) +
+    theme_bw() +
+    theme(panel.spacing.x = unit(6, "mm"),
+          legend.position="bottom",
+          plot.title = element_text(hjust = 0.5))
+   }
   
   if(spec == "Adaptation") {
     
+    # Decile-level impacts
     title <- expression(atop(bold("Damages at decile-level, across countries")))
 
     
@@ -234,13 +406,51 @@ decile_plot = function (spec, dir) {
             legend.position="bottom",
             plot.title = element_text(hjust = 0.5))
     
+    # Avoided decile-level impacts from REF to 650
+    title_2 <- expression(atop(bold("Avoided damages at decile-level from REF to 650")))
     
+    p_dec_avoided_dam <-  ggplot(mip_income_d %>% 
+                                   na.omit() %>% 
+                                   filter(Scenario == "REF" | Scenario == "650") %>% 
+                                   select(Scenario:Year, Decile, dam_dist_frac_ada) %>% 
+                                   select(-Scenario_type) %>% 
+                                   distinct() %>% 
+                                   mutate(
+                                     dam_dist_frac_bhm = dam_dist_frac_ada*100,
+                                     Decile = factor(Decile,
+                                                     levels = c("D1", "D2", "D3", "D4", "D5",
+                                                                "D6", "D7", "D8", "D9", "D10"))
+                                   ) %>% 
+                                   pivot_wider(
+                                     names_from = Scenario,
+                                     values_from = dam_dist_frac_ada
+                                   ) %>% 
+                                   mutate(avoided_reg_dam_ada = REF - `650`) ,
+                                 aes(x = Year, y = avoided_reg_dam_ada,
+                                     color = Decile, shape = Model)) +
+      geom_line(alpha = 0.5) +
+      geom_point() +
+      labs(x = "Year", y = "Avoided damages, %", title = title_2) +
+      scale_color_manual(name = "Income deciles", values = colors_deciles) +
+      scale_shape_manual(name = "Models", values = mod_letters_utf) +
+      facet_wrap(~ Region, ncol = 5) +
+      scale_x_continuous(limits = c(2020, 2100), breaks = c(2020, 2060, 2100)) +
+      theme_bw() +
+      theme(panel.spacing.x = unit(6, "mm"),
+            legend.position="bottom",
+            plot.title = element_text(hjust = 0.5))
   }
   
   ggsave(filename = paste0(spec, "_decile_damages.png"),
+         plot = p_dec_dam,
          width = 15, height = 7, path = dir)
   
-  return(p_dec_dam)
+  ggsave(filename = paste0(spec, "_decile_avoided_damages.png"),
+         plot = p_dec_avoided_dam,
+         width = 15, height = 7, path = dir)
+  
+  
+  return(list(p_dec_dam, p_dec_avoided_dam))
   
 }
 
